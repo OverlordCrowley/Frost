09fefbf229a1b62cd2b90d7b6caa7e34e5980479
OverlordCrowley <maksim2004@bk.ru>
2023-07-20 20:03:24 +0600

    Pre-final version

--- frost-frontend/src/Components/History/HistoryBlock.tsx
+++ frost-frontend/src/Components/History/HistoryBlock.tsx
@@ -30,35 +30,40 @@ const HistoryBlock = () => {
         <div>
             <div className="profile-small history-small">
                 <p className="orderItem-text">История заказов</p>
-                <thead>
-                <tr className="orderItem-rows">
-                    <th className="th orderItem-number">Номер заказа</th>
-                    <th className="th orderItem-name">Наименование товара</th>
-                    <th className="th orderItem-date">Дата заказа</th>
-                    <th className="th orderItem-price">Стоимость</th>
-                </tr>
-                </thead>
-                <tbody>
-                {items && items.length > 0 ? items.map((el,index)=>(
-                    <tr className="orderItem-row" key={index}>
-                        <td className="orderItem-row__text orderItem-row__text-code">
-                            <b>{el.orderId}</b>
-                        </td>
-                        <td className="orderItem-row__text orderItem-row__text-pd">
-                            {el.devices && el.devices.map((el2: any, index2: number) => (
-                                <div key={index2}>
-                                    <p key={index2}>{el2.device.name}</p>
-                                    <p style={{marginTop: 10}}>{el2.count} X {el2.device.price}</p>
-                                </div>
-                            ))}
-                        </td>
-                        <td className="orderItem-row__text orderItem-date">{el.updatedAt}</td>
-                        <td className="orderItem-row__text orderItem-date">{el.totalCost} тг</td>
+                <table>
+                    <thead>
+                    <tr className="orderItem-rows">
+                        <th className="th orderItem-number">Номер заказа</th>
+                        <th className="th orderItem-name">Наименование товара</th>
+                        <th className="th orderItem-date">Дата заказа</th>
+                        <th className="th orderItem-price">Стоимость</th>
                     </tr>
+                    </thead>
+                    <tbody>
+                    {items && items.length > 0 ? items.map((el, index) => (
+                        <tr className="orderItem-row" key={index}>
+                            <td className="orderItem-row__text orderItem-row__text-code">
+                                <b>{el.orderId}</b>
+                            </td>
+                            <td className="orderItem-row__text orderItem-row__text-pd">
+                                {el.devices && el.devices.map((el2: any, index2: number) => (
+                                    <div key={index2}>
+                                        <p>{el2.device.name}</p>
+                                        <p style={{marginTop: 10}}>{el2.count} X {el2.device.price}</p>
+                                    </div>
+                                ))}
+                            </td>
+                            <td className="orderItem-row__text orderItem-date">{el.updatedAt}</td>
+                            <td className="orderItem-row__text orderItem-date">{el.totalCost} тг</td>
+                        </tr>
+                    )) : (
+                        <tr>
+                            <td>Список истории заказов пуст</td>
+                        </tr>
+                    )}
+                    </tbody>
+                </table>
 
-
-                )) : 'Список истории заказов пуст'}
-                </tbody>
             </div>
 
         </div>
